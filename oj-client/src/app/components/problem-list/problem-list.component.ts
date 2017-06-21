32d40a72020034de085806781cf5d3abd9a0d968
Rita <rita.ren.yuxi@wishlife.com>
2017-06-21 15:11:42 -0700

    use ng g c problem-list -it -is to create a component

---
+++ oj-client/src/app/components/problem-list/problem-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-problem-list',
+  template: `
+    <p>
+      problem-list Works!
+    </p>
+  `,
+  styles: []
+})
+export class ProblemListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
