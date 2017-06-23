831f912eee83feee4e971bea5abdd69b019f68e9
yuxiRen <rita.ren.yuxi@gmail.com>
2017-06-23 14:57:33 -0700

    import route

--- oj-client/src/app/app.module.ts
+++ oj-client/src/app/app.module.ts
@@ -2,20 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { routing} from './app.routes';
 import { AppComponent } from './app.component';
 import { ProblemListComponent } from './components/problem-list/problem-list.component';
 import { DataService } from './services/data.service';
+import { ProblemDetailComponent } from './components/problem-detail/problem-detail.component';
 @NgModule({
   declarations: [
     AppComponent,
     ProblemListComponent,
-    ProblemListComponent
+    ProblemListComponent,
+    ProblemDetailComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    routing
   ],
   providers: [{provide: 'data',useClass: DataService}],
   bootstrap: [AppComponent]
