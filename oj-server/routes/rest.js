a9dfc5bdf444eab2e8b86a6044baac13ff6d77e0
yuxiRen <rita.ren.yuxi@gmail.com>
2017-06-29 20:42:52 -0700

    add build route

--- oj-server/routes/rest.js
+++ oj-server/routes/rest.js
@@ -4,6 +4,10 @@ var bodyParser = require('body-parser');
 var jsonParser = bodyParser.json();
 
 var problemService = require('../services/problemService');
+var nodeRestClient = require('node-rest-client').Client;
+var restClient = new nodeRestClient();
+EXECUTOR_SERVER_URL = "http://localhost:5000/build_and_run";
+restClient.registerMethod('build_and_run', EXECUTOR_SERVER_URL, 'POST');
 
 router.get('/problems', function(req, res) {
     problemService.getProblems()
@@ -28,9 +32,6 @@ router.post('/problems', jsonParser, function(req, res) {
 router.post('/build_and_run', jsonParser, function(req, res) {
     const userCode = req.body.userCode;
     const lang = req.body.lang;
-    console.log('lang: ' + lang + '&&&& userCode: ' + userCode);
-
-    // res.json({'text': 'hello from nodejs hahahaha'});
     restClient.methods.build_and_run(
         {
             data: {
